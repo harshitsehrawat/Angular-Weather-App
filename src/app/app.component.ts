138f55ce163a8614534fa5ce97de600284667077
harshit <harshit@harshit.com>
2022-10-18 16:37:41 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-show-api',
+templateUrl: './show-api.component.html',
+styleUrls: ['./show-api.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class ShowApiComponent implements OnInit {
+
+li:any;
+lis=[];
+constructor(private http : HttpClient){
+	
 }
+
+ngOnInit(): void {
+	this.http.get('https://countriesnow.space/api/v0.1/countries')
+	.subscribe(Response => {
+
+	// If response comes hideloader() function is called
+	// to hide that loader
+	if(Response){
+		hideloader();
+	}
+	console.log(Response)
+	this.li=Response;
+	this.lis=this.li.list;
+	});
+	function hideloader(){
+	// document.getElementById('loading').style.display = 'none';}
+}}
+// The url of api is passed to get() and then subscribed and
+// stored the response to li element data array list[] is created
+// using JSON element property
+
+// export class AppComponent {
+//   title = 'my-app';
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import {RouterModule} from '@angular/router';
+import {allAppRoutes} from './routes';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(allAppRoutes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
